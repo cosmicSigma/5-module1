db0815e523ae63f832dd1972a3b8c4f1339c3f4f
cosmicSigma <abdurashidxodiyev17@gmail.com>
2023-04-13 10:22:22 +0500

    tested

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-
+  MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Image(
+            image: AssetImage("assets/img/img_1.jpg"),
+            ),
           ),
         ),
       ),
-    )
   );
 }
+
+// ff0084
+// 33001b
