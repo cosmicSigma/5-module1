a2e9a20e2b001e20de7579e808d0d54aff5551a8
cosmicSigma <abdurashidxodiyev17@gmail.com>
2023-04-13 10:42:30 +0500

    img

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-  MaterialApp(
+  const MaterialApp(
     home: Scaffold(
       body: Center(
         child: Image(
-            image: AssetImage("assets/img/img_1.jpg"),
+            image: AssetImage("assets/img/img.png"),
+            width: 300,
+          height: 500,
+          fit: BoxFit.cover,
             ),
           ),
         ),
